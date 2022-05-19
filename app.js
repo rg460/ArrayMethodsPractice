fa6b01a4a1640c3febb2e79736ecf770911c1e72
ravigordon <rgordz460@outlook.com>
2022-05-19 14:11:36 +0100

    sorting iventors from oldest to youngest

--- app.js
+++ app.js
@@ -55,6 +55,17 @@ const inventors = [
   console.log(totalYears);
   // 5. Sort the inventors by years lived
 
+  const oldest = inventors.sort(function(a,b){
+        const lastGuy = a.passed - a.year;
+        const nextGuy = a.passed + a.year;
+        if(lastGuy > nextGuy){
+            return -1;  
+        } else{
+            return 1;
+        }
+  });
+  console.log(oldest);
+
   // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
   // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
