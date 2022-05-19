d0886bd3e96b0e3358439b61bc598496ccf44e4d
ravigordon <rgordz460@outlook.com>
2022-05-19 13:45:01 +0100

    .map example giving an array of the first and last names of the iventors

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ const inventors = [
     console.table(fifteen);
   // Array.prototype.map()
   // 2. Give us an array of the inventors first and last names
+  const fullNames = inventors.map(function(inventor){
+        return inventors.first + inventors.last;
+  })
+  console.log(fullNames);
 
   // Array.prototype.sort()
   // 3. Sort the inventors by birthdate, oldest to youngest
