f78e9d21cbcbb78eb608cf15f983fd39cf4d5644
ravigordon <rgordz460@outlook.com>
2022-05-20 16:20:17 +0100

    transportation variable made

--- app.js
+++ app.js
@@ -87,3 +87,6 @@ const inventors = [
   // 8. Reduce Exercise
   // Sum up the instances of each of these
   const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+const transportation = data.reduce(function(obj,item){
+    
+})
