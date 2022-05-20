1a336e7fc3ed07733de5bc323579c065c5963a38
ravigordon <rgordz460@outlook.com>
2022-05-20 16:22:57 +0100

    blank object set up

--- app.js
+++ app.js
@@ -88,5 +88,7 @@ const inventors = [
   // Sum up the instances of each of these
   const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
 const transportation = data.reduce(function(obj,item){
+
+},{
     
 })
